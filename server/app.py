30fdf24771a90bc251bc18aaec7b4316427427d3
LidyaGhilay <lidyahenok9@gmail.com>
2024-07-06 23:04:48 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,5 +1,4 @@
-# server/app.py
-#!/usr/bin/env python3
+
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
@@ -13,19 +12,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     return make_response(
         '<h1>Welcome to the pet directory!</h1>',
         200
     )
-
-
-@app.route('/demo_json')
-def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
 
 
 if __name__ == '__main__':
